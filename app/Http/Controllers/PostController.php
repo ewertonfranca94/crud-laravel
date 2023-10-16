e4926eb73f1056e5e293e1f475dcc7797186885f
Ewerton <ewerton-1994@hotmail.com>
2023-10-16 03:08:54 -0300

    crud simples funcionando

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,8 +3,64 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\models\Post;
 
 class PostController extends Controller
 {
     //
+    public function create(Request $request){
+        // Forma convencional de Create
+        // $new_post = [
+        //     'title' => 'Meu primeiro post',
+        //     'content' => 'conteuto do post 1',
+        //     'author'=> 'ewerton'
+        // ];  
+        
+        // $post = new Post($new_post);
+        // $post->save();
+
+        //Forma 2 - de usar CREATE
+
+        // $post = new Post();
+
+        // $post->title = 'meu segundo post';
+        // $post->content = 'conteudo do meu segundo post';
+        // $post->author = 'Evinho';
+        // $post->save();
+        // dd($post);
+
+        // metodo de CREATE mais compacto sem usar save()    
+        $post = Post::create([
+            'title' => 'Novo Título',
+            'content' => 'Conteúdo do post chat',
+            'author' => 'Autor'
+        ]);
+    }
+
+    public function read(Request $r){
+        $post = new Post();
+
+        $post = $post->find(1);
+
+        return $post;
+    }
+    public function all(Request $r){
+        $posts = Post::all();
+        return $posts;
+    }
+    public function update(Request $request){
+        // $post = Post::find(1); //pega um post com base no id
+        $posts = Post::where('id', '>',0)->update([
+            'author' => 'Desconhecido'
+        ]);
+        // $post->title = 'Primeiro Post ATUALIZADO';
+        $posts->save();
+
+        return $posts;
+
+    }
+    public function delete(Request $r){
+        $post = Post::find(7); //recupera postagem com base no id
+        $post->delete(); // deleta postagem
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+
+Route::get('posts/create', [PostController::class,'create']);
+Route::get('posts/read', [PostController::class,'read']);
+Route::get('posts/all', [PostController::class,'all']);
+Route::get('posts/update', [PostController::class,'update']);
+Route::get('posts/delete', [PostController::class,'delete']);
 
 Route::get('/', function () {
     return view('welcome');
